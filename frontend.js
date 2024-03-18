12d4820f6888de9fb331ab38cbe9b7495d6d5917
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2024-03-18 20:55:20 +0800

    添加对网页端的检测规则

--- frontend.js
+++ frontend.js
@@ -13,14 +13,22 @@
 // 下一个目标：部署到服务器
 (function () {
   ("use strict");
-  const detect_string = "Hi,你有新的课堂习题";
+  const run_detect = (to_be_detected, detect_string_list) => {
+    for (let i = 0; i < detect_string_list.length; i++) {
+      if (to_be_detected.includes(detect_string_list[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+  const detect_string_list = ["Hi,你有新的课堂习题", '提交答案', '倒计时'];
   var post_url = ".env";
-  function checkContentAndAdjustInterval(searchString, initialDelay, newDelay) {
+  function checkContentAndAdjustInterval(detect_string_list, initialDelay, newDelay) {
     // 定义一个函数，用于检查页面内容并根据条件调整定时器
     function checkAndSchedule() {
       var pageText = document.body.innerText;
       console.log('detecting string');
-      if (pageText.includes(detect_string)) {
+      if (run_detect(pageText, detect_string_list)) {
         console.log("found string");
         const data = new FormData();
         data.append("data", "yuketang");
@@ -46,5 +54,5 @@
 
   // 使用示例：每隔10秒检查一次页面是否包含"特定字符串"，
   // 如果找到，则下次检查的时间间隔变为100秒
-  checkContentAndAdjustInterval(detect_string, 2000, 10000);
+  checkContentAndAdjustInterval(detect_string_list, 2000, 10000);
 })();
